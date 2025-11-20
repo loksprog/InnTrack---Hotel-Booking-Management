2732f1998d7bb815e9488b97eb8abf5248e9e3bb
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2025-11-20 14:28:08 +0800

    feat: add our reach us and footer section

--- index.php
+++ index.php
@@ -7,10 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
   <link rel="stylesheet" href="css/common.css">
@@ -29,13 +26,17 @@
         margin-top: 25px;
         padding: 0 35px;
       }
+
+      .crt {
+        font-size: 0.75rem;
+      }
     }
   </style>
 </head>
 
 <body class="bg-light">
   <!-- Navigation Bar -->
-  <nav class="navbar navbar-expand-lg bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+  <nav class="navbar navbar-expand-lg bg-light px-lg-3 py-lg-2 sticky-top">
     <div class="container-fluid">
       <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">InnTrack</a>
       <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -63,7 +64,7 @@
           <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
             Login
           </button>
-          <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
+          <button type="button" class="btn custom-bg text-white shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
             Register
           </button>
         </div>
@@ -195,7 +196,7 @@
   <!-- Check Availability Form -->
   <div class="container availability-form">
     <div class="row">
-      <div class="col-lg-12 bg-white shadow p-4 rounded">
+      <div class="col-lg-12 bg-white shadow p-4 rounded-4">
         <h5 class="mb-4">Check Booking Availability</h5>
         <form>
           <div class="row align-items-end">
@@ -240,9 +241,8 @@
   <div class="container">
     <div class="row">
       <!-- card 1 -->
-      <div class="col-lg-4 col-md-6 my-3 ">
-
-        <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+      <div class="col-lg-4 col-md-6 my-3">
+        <div class="card rounded-4 overflow-hidden" style="max-width: 350px; margin: auto; border: 2px solid #2ec1ab42;">
           <img src="images/rooms/1.jpg" class="card-img-top" alt="...">
 
           <div class="card-body">
@@ -296,13 +296,12 @@
           </div>
 
         </div>
-
       </div>
 
       <!-- card 2 -->
       <div class="col-lg-4 col-md-6 my-3 ">
 
-        <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+        <div class="card rounded-4 overflow-hidden" style="max-width: 350px; margin: auto; border: 2px solid #2ec1ab42;">
           <img src="images/rooms/2.png" class="card-img-top" alt="...">
 
           <div class="card-body">
@@ -362,7 +361,7 @@
       <!-- card 3-->
       <div class="col-lg-4 col-md-6 my-3 ">
 
-        <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+        <div class="card rounded-4 overflow-hidden" style="max-width: 350px; margin: auto; border: 2px solid #2ec1ab42;">
           <img src="images/rooms/3.png" class="card-img-top" alt="...">
 
           <div class="card-body">
@@ -430,28 +429,31 @@
 
   <div class="container">
     <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
-      <div class="col-lg-2 col-md-2 text-center bg-whtie rounded shadow py-4 my-3">
+      <div class="col-lg-2 col-md-2 text-center bg-whtie rounded-4 py-4 my-3" style="border: 2px solid #2ec1ab42;">
         <img src="images/features/wifi.svg" width="80px" alt="">
         <h5 class="mt-3">Wifi</h5>
       </div>
-      <div class="col-lg-2 col-md-2 text-center bg-whtie rounded shadow py-4 my-3">
+      <div class="col-lg-2 col-md-2 text-center bg-whtie rounded-4 py-4 my-3" style="border: 2px solid #2ec1ab42;">
         <img src="images/features/wifi.svg" width="80px" alt="">
         <h5 class="mt-3">Wifi</h5>
       </div>
-      <div class="col-lg-2 col-md-2 text-center bg-whtie rounded shadow py-4 my-3">
+      <div class="col-lg-2 col-md-2 text-center bg-whtie rounded-4 py-4 my-3" style="border: 2px solid #2ec1ab42;">
         <img src="images/features/wifi.svg" width="80px" alt="">
         <h5 class="mt-3">Wifi</h5>
       </div>
-      <div class="col-lg-2 col-md-2 text-center bg-whtie rounded shadow py-4 my-3">
+      <div class="col-lg-2 col-md-2 text-center bg-whtie rounded-4 py-4 my-3" style="border: 2px solid #2ec1ab42;">
         <img src="images/features/wifi.svg" width="80px" alt="">
         <h5 class="mt-3">Wifi</h5>
       </div>
-      <div class="col-lg-2 col-md-2 text-center bg-whtie rounded shadow py-4 my-3">
+      <div class="col-lg-2 col-md-2 text-center bg-whtie rounded-4 py-4 my-3" style="border: 2px solid #2ec1ab42;">
         <img src="images/features/wifi.svg" width="80px" alt="">
         <h5 class="mt-3">Wifi</h5>
       </div>
+
       <div class="col-lg-12 text-center mt-5">
-        <a href="" class="btn btn-sm btn-outline-dark rounded  fw-bold shadow-none">More Facilities ></a>
+        <a href="" class="btn btn-sm btn-outline-dark rounded  fw-bold shadow-none">
+          More Facilities >
+        </a>
       </div>
     </div>
   </div>
@@ -459,11 +461,11 @@
   <!-- Testimonials -->
   <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Testimonials</h2>
 
-  <div class="container">
+  <div class="container mt-5">
     <div class="swiper swiper-testimonials">
       <div class="swiper-wrapper mb-5">
 
-        <div class="swiper-slide bg-white p-4 rounded overflow-hidden">
+        <div class="swiper-slide bg-white p-4 rounded-4 overflow-hidden">
           <div class="profile d-flex align-items-center mb-3 ">
             <img src="images/features/star.svg" width="30px">
             <h6 class="m-0 ms-2">Random user1</h6>
@@ -482,7 +484,7 @@
           </div>
         </div>
 
-        <div class="swiper-slide bg-white p-4 rounded overflow-hidden">
+        <div class="swiper-slide bg-white p-4 rounded-4 overflow-hidden">
           <div class="profile d-flex align-items-center mb-3">
             <img src="images/features/star.svg" width="30px">
             <h6 class="m-0 ms-2">Random user1</h6>
@@ -501,7 +503,7 @@
           </div>
         </div>
 
-        <div class="swiper-slide bg-white p-4 rounded overflow-hidden">
+        <div class="swiper-slide bg-white p-4 rounded-4 overflow-hidden">
           <div class="profile d-flex align-items-center mb-3">
             <img src="images/features/star.svg" width="30px">
             <h6 class="m-0 ms-2">Random user1</h6>
@@ -520,7 +522,7 @@
           </div>
         </div>
 
-        <div class="swiper-slide bg-white p-4 rounded overflow-hidden">
+        <div class="swiper-slide bg-white p-4 rounded-4 overflow-hidden">
           <div class="profile d-flex align-items-center mb-3">
             <img src="images/features/star.svg" width="30px">
             <h6 class="m-0 ms-2">Random user1</h6>
@@ -544,10 +546,94 @@
   </div>
 
 
+  <!-- Reach Us -->
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Reach Us</h2>
 
-  <br><br><br>
-  <br><br><br>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded-4">
+        <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3925.5592376914806!2d123.89408907583505!3d10.297052967851192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a99bfd54c152e5%3A0xf9b2f3f4bd996da0!2sUniversity%20of%20Cebu%20-%20Main%20Campus!5e0!3m2!1sen!2sph!4v1763606801558!5m2!1sen!2sph" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <div class="col-lg-4 col-md-4">
 
+        <!-- Call us -->
+        <div class="bg-white p-4 rounded-4 mb-4">
+          <h5>Call us</h5>
+          <a href="tel: +63922335842" class="d-inline-block mb-2 text-decoration-none text-dark">
+            <i class="bi bi-telephone-fill"></i>
+            +63922335842
+          </a>
+          <br>
+          <a href="tel: +63922335842" class="d-inline-block mb-2 text-decoration-none text-dark">
+            <i class="bi bi-telephone-fill"></i>
+            +63922335842
+          </a>
+        </div>
+
+        <!-- Follow  us -->
+        <div class="bg-white p-4 rounded-4 mb-0">
+          <h5>Follow us</h5>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge text-dark fs-6 p-2" style="background-color: #2ec1ab42;">
+              <i class="bi bi-facebook me-1"></i>
+              Facebook
+            </span>
+          </a>
+          <br>
+          <a href="#" class="d-inline-block mb-3">
+            <span class="badge text-dark fs-6 p-2" style="background-color: #2ec1ab42;">
+              <i class="bi bi-instagram me-1"></i>
+              Instagram
+            </span>
+          </a>
+          <br>
+          <a href="#" class="d-inline-block">
+            <span class="badge text-dark fs-6 p-2" style="background-color: #2ec1ab42;">
+              <i class="bi bi-tiktok me-1"></i>
+              TikTok
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Footer -->
+  <div class="container-fluid bg-white mt-5">
+    <div class="row">
+      <div class="col-lg-4 p-4">
+        <h3 class="h-font fw-bold fs-3 mb-2">InnTrack</h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur molestias laudantium aut distinctio provident similique quisquam ad sit voluptates, libero, error ex corporis, id sequi expedita dignissimos maxime quasi rem?
+        </p>
+      </div>
+      <div class="col-lg-4 p-4">
+        <h5 class="mb-3">Links</h5>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a> <br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+      </div>
+      <div class="col-lg-4 p-4">
+        <h5 class="mb-3">Follow us</h5>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-facebook me-1"></i>
+          Facebook
+        </a> <br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-instagram me-1"></i>
+          Instagram
+        </a> <br>
+        <a href="#" class="d-inline-block text-dark text-decoration-none">
+          <i class="bi bi-tiktok me-1"></i>
+          TikTok
+        </a> <br>
+      </div>
+    </div>
+  </div>
+
+  <div class="text-center bg-dark text-white crt fs-md-6 fs-lg-6 p-3 m-0">Developed by Group 1</div>
 
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
