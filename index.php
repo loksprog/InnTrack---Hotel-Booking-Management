00ba2afb089450a82d9542af48e244a5da32f02d
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2025-11-30 21:59:19 +0800

    refactor: clean up code and move files into separate directories

--- index.php
+++ index.php
@@ -4,14 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  <?php require('sections/links.php') ?>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
-  <link rel="stylesheet" href="css/common.css">
-
   <title>InnTrack</title>
 
   <style>
@@ -469,45 +463,10 @@
     </div>
   </div>
 
-  <!-- Footer -->
-  <div class="container-fluid bg-white mt-5">
-    <div class="row">
-      <div class="col-lg-4 p-4">
-        <h3 class="h-font fw-bold fs-3 mb-2">InnTrack</h3>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur molestias laudantium aut distinctio provident similique quisquam ad sit voluptates, libero, error ex corporis, id sequi expedita dignissimos maxime quasi rem?
-        </p>
-      </div>
-      <div class="col-lg-4 p-4">
-        <h5 class="mb-3">Links</h5>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a> <br>
-        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
-      </div>
-      <div class="col-lg-4 p-4">
-        <h5 class="mb-3">Follow us</h5>
-        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
-          <i class="bi bi-facebook me-1"></i>
-          Facebook
-        </a> <br>
-        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
-          <i class="bi bi-instagram me-1"></i>
-          Instagram
-        </a> <br>
-        <a href="#" class="d-inline-block text-dark text-decoration-none">
-          <i class="bi bi-tiktok me-1"></i>
-          TikTok
-        </a> <br>
-      </div>
-    </div>
-  </div>
 
-  <div class="text-center bg-dark text-white crt fs-md-6 fs-lg-6 p-3 m-0">Developed by Group 1</div>
+  <?php require('sections/footer.php') ?>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
 
   <!-- Hero images -->
---
+++ sections/footer.php
@@ -0,0 +1,37 @@
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+        <div class="col-lg-4 p-4">
+            <h3 class="h-font fw-bold fs-3 mb-2">InnTrack</h3>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur molestias laudantium aut distinctio provident similique quisquam ad sit voluptates, libero, error ex corporis, id sequi expedita dignissimos maxime quasi rem?
+            </p>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Links</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a> <br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Follow us</h5>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-facebook me-1"></i>
+                Facebook
+            </a> <br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-instagram me-1"></i>
+                Instagram
+            </a> <br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none">
+                <i class="bi bi-tiktok me-1"></i>
+                TikTok
+            </a> <br>
+        </div>
+    </div>
+</div>
+
+<div class="text-center bg-dark text-white crt fs-md-6 fs-lg-6 p-3 m-0">Developed by Group 1</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
