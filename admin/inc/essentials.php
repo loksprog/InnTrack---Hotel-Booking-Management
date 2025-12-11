c82d856fca80f619aa2850cd3a2fb93437904613
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2025-12-12 04:01:19 +0800

    fix: fixed svg path

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -5,12 +5,14 @@
 define('SITE_URL', 'http://127.0.0.1/inntrack/');
 define('ABOUT_IMG_PATH', SITE_URL . 'images/about/');
 define('CAROUSEL_IMG_PATH', SITE_URL . 'images/carousel/');
+define('FEATURES_IMG_PATH', SITE_URL . 'images/features/');
 
 //backend upload process needs this data
 
 define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/InnTrack/images/');
 define('ABOUT_FOLDER', 'about/');
 define('CAROUSEL_FOLDER', 'carousel/');
+define('FEATURES_FOLDER', 'features/');
 
 function adminLogin()
 {
@@ -72,5 +74,27 @@ function deleteImage($image, $folder)
         return true;
     } else {
         return false;
+    }
+}
+
+function uploadSVGImage($image, $folder)
+{
+    $valid_mime = ['image/svg+xml'];
+    $img_mime = $image['type'];
+
+    if (!in_array($img_mime, $valid_mime)) {
+        return 'inv_img'; //invalid image mime or format
+    } else if (($image['size'] / (1024 * 1024)) > 1) {
+        return 'inv_size'; //invalid size greater than 1mb
+    } else {
+        $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
+        $rname = 'IMG_' . random_int(11111, 99999) . ".$ext";
+
+        $img_path = UPLOAD_IMAGE_PATH . $folder . $rname;
+        if (move_uploaded_file($image['tmp_name'], $img_path)) {
+            return $rname;
+        } else {
+            return 'upd_failed';
+        }
     }
 }
