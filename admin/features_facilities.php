a1d38e611d11e962183cf9a248fb26ca169bfdb4
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2025-12-12 04:35:13 +0800

    feat: added facilities section

--- admin/features_facilities.php
+++ admin/features_facilities.php
@@ -104,12 +104,43 @@ if (isset($_GET['del'])) {
                     </div>
                 </div>
 
+                <div class="card border-0 shadow-sm mb-4">
+                    <div class="card-body">
+
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h5 class="card-title m-0">Facilities</h5>
+                            <button type="button" class="btn btn-dark btn-sm" data-bs-toggle="modal" data-bs-target="#facility-s">
+                                <i class="bi bi-plus-square"></i> Add
+                            </button>
+                        </div>
+
+
+                        <div class="table-responsive-md" style="height: 350px; overflow-y:scroll;">
+                            <table class="table table-hover border">
+                                <thead>
+                                    <tr class="bg-dark text-light">
+                                        <th scope="col">#</th>
+                                        <th scope="col">Icon</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col" width="40%">Description</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="facilities-data">
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
 
 
     <!-- Feature Modal -->
+
     <div class="modal fade" id="feature-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
             <form id="feature_s_form">
@@ -132,10 +163,44 @@ if (isset($_GET['del'])) {
         </div>
     </div>
 
+    <!-- Facility Modal -->
+
+    <div class="modal fade" id="facility-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <form id="facility_s_form">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Add Facility</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label class="form-label fw-bold">Name</label>
+                            <input type="text" name="facility_name" class="form-control shadow-none" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label fw-bold">Picture</label>
+                            <input type="file" name="facility_icon" accept=".svg" class="form-control shadow-none" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Description</label>
+                            <textarea name="facility_desc" class="form-control shadow-none" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="reset" class="btn text-secondary shadow-none" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn custom-bg text-white shadow-none">Add Facility</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
     <?php require('inc/scripts.php'); ?>
 
     <script>
         let feature_s_form = document.getElementById("feature_s_form");
+        let facility_s_form = document.getElementById("facility_s_form");
 
         feature_s_form.addEventListener("submit", function(e) {
             e.preventDefault();
@@ -189,8 +254,7 @@ if (isset($_GET['del'])) {
                     get_features();
                 } else if (this.responseText == 'room.added') {
                     aler('error', 'Feature is added in room!');
-                } 
-                else {
+                } else {
                     alert("error", "Server down!");
                 }
             };
@@ -198,9 +262,75 @@ if (isset($_GET['del'])) {
             xhr.send("rem_feature=" + val);
         }
 
+        facility_s_form.addEventListener("submit", function(e) {
+            e.preventDefault();
+            add_facility();
+        });
+
+        function add_facility() {
+            let data = new FormData();
+            data.append("name", facility_s_form.elements['facility_name'].value);
+            data.append("icon", facility_s_form.elements['facility_icon'].files[0]);
+            data.append("desc", facility_s_form.elements['facility_desc'].value);
+            data.append("add_facility", "");
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/features_facilities.php", true);
+
+            xhr.onload = function() {
+                let modalElement = document.getElementById("facility-s");
+                let modal = bootstrap.Modal.getInstance(modalElement);
+                modal.hide();
+
+                if (this.responseText == "inv_img") {
+                    alert("error", "Only SVG images are allowed!");
+                } else if (this.responseText == "inv_size") {
+                    alert("error", "Image should be less than 1MB!");
+                } else if (this.responseText == "upd_failed") {
+                    alert("error", "Image upload failed!");
+                } else {
+                    alert("success", "New facility added!");
+                    facility_s_form.reset();
+                    get_facilities();
+                }
+            };
+
+            xhr.send(data);
+        }
+
+        function get_facilities() {
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/features_facilities.php", true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            xhr.onload = function() {
+                document.getElementById("facilities-data").innerHTML = this.responseText;
+            };
+            xhr.send("get_facilities");
+        }
+
+        function rem_facility(val) {
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/features_facilities.php", true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            xhr.onload = function() {
+                if (this.responseText == 1) {
+                    alert("success", "Facility removed!");
+                    get_facilities();
+                } else if (this.responseText == 'room.added') {
+                    aler('error', 'Facility is added in room!');
+                } else {
+                    alert("error", "Server down!");
+                }
+            };
+
+            xhr.send("rem_facility=" + val);
+        }
 
         window.onload = function() {
             get_features();
+            get_facilities();
         }
     </script>
 
